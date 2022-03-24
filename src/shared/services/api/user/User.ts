1717a44a06c16202a8772ecc08c35140ea8fd609
Leonardo da Silva <silvaleonardo.ti@gmail.com>
2022-03-24 18:50:54 -0300

    mocking signIn and signUp methods

--- src/shared/services/api/user/User.ts
+++ src/shared/services/api/user/User.ts
@@ -1,24 +1,60 @@
 import { IRequestResult } from "../../../interfaces/IRequestResult";
 
-export interface IUser{
+export interface IUser {
     id:string;
     username: string;
     firstName: string;
     lastName: string;
     email: string;
-    links: string[];
+    links?: string[];
+}
+export interface IUserSignUpReturn extends Omit<IUser, 'links'> {
     token: string
     password:string;
 }
 
-
-
-const signIn = (email: string, password: string): Promise<IRequestResult<IUser>> => {
-    return new Promise<IRequestResult>((resolve, reject) => {})
+export interface IUserSignInReturn extends Omit<IUser, 'links'> {
+    token: string
+    password:string;
 }
 
-const signUp = (user: IUser): Promise<IRequestResult<IUser>> => {
-    return new Promise<IRequestResult>((resolve, reject) => {})
+const signIn = async (email: string, password: string): Promise<IRequestResult<IUserSignInReturn>> => {
+    
+        const result = {
+            data: {
+                id: "1",
+                username: "leosilva",
+                firstName: "Leonardo",
+                lastName: "Silva",
+                email: email,
+                links: [''],
+                token: 'jwt-123456',
+                password: password,
+            } as IUserSignInReturn,
+            success: true,
+            messages: [],
+        } as IRequestResult<IUserSignInReturn> 
+        
+        return result;
+
+}
+
+const signUp = async (user: Omit<IUser, 'id'|'links'>): Promise<IRequestResult<IUserSignUpReturn>> => {
+    const result = {
+        data: {
+            id:'1', /* dados fakes que virão do backend */
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            token: 'jwt-123456', /* dados fakes que virão do backend */
+            password: '123456', /* dados fakes que virão do backend */
+        } as IUserSignUpReturn,
+        success: true,
+        messages: [],
+    } as IRequestResult<IUserSignUpReturn> 
+    
+    return result;
 }
 
 
