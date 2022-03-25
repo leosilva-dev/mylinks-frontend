d6315495f4f2731719948c21e0831cb7d1291601
Leonardo da Silva <silvaleonardo.ti@gmail.com>
2022-03-25 17:07:07 -0300

    Refactoring and exposing methods to change context state

--- src/shared/contexts/UserContext.tsx
+++ src/shared/contexts/UserContext.tsx
@@ -1,35 +1,58 @@
-import React, { createContext, useCallback, useEffect, useState } from "react";
-import { IUser, userService } from "../services/api/user/User";
-import { Api } from "../services/axios-config/AxiosConfig";
-import { Feedback } from "../services/feedback/Feedback";
+import React, { createContext, useCallback, useEffect, useState } from 'react';
+import { IUser, userService } from '../services/api/user/User';
+import { Api } from '../services/axios-config/AxiosConfig';
+import { Feedback } from '../services/feedback/Feedback';
 
 interface IUserContextData {
-  user: IUser;
+  id: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  description: string;
   authenticated: boolean;
   isLoading: boolean;
   handleLogin: (email: string, password: string) => void;
   handleLogout: () => void;
   handleSignUp: (user: IUser) => void;
+  defineUserFirstName: (value: string) => void;
+  defineUserLastName: (value: string) => void;
+  defineUserEmail: (value: string) => void;
+  defineUserUsername: (value: string) => void;
+  defineUserDescription: (value: string) => void;
 }
 export const UserContext = createContext<IUserContextData>(
-  {} as IUserContextData
+  {} as IUserContextData,
 );
 
 export const UserProvider: React.FC = ({ children }) => {
-  const [user, setUser] = useState<IUser>({} as IUser);
+  const [id, setId] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [description, setDescription] = useState('');
+
   const [authenticated, setAuthenticated] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token');
 
     if (token) {
-      Api.defaults.headers.common["Authorization"] = `Bearer ${JSON.parse(
-        token
+      Api.defaults.headers.common['Authorization'] = `Bearer ${JSON.parse(
+        token,
       )}`;
 
       userService.getUserByToken(token).then((response) => {
-        setUser(response.data);
+        DefineUser(
+          response.data.id,
+          response.data.firstName,
+          response.data.lastName,
+          response.data.username,
+          response.data.email,
+          response.data.description || '',
+        );
       });
 
       setAuthenticated(true);
@@ -43,22 +66,29 @@ export const UserProvider: React.FC = ({ children }) => {
 
     const response = await userService.signIn(email, password);
     if (response.success) {
-      localStorage.setItem("token", JSON.stringify(response.token));
-      Api.defaults.headers.common["Authorization"] = `Bearer ${response.token}`;
-      setUser(response.data);
+      localStorage.setItem('token', JSON.stringify(response.token));
+      Api.defaults.headers.common['Authorization'] = `Bearer ${response.token}`;
+      DefineUser(
+        response.data.id,
+        response.data.firstName,
+        response.data.lastName,
+        response.data.username,
+        response.data.email,
+        response.data.description || '',
+      );
       setAuthenticated(true);
-      Feedback("Login realizado com sucesso!", "success");
+      Feedback('Login realizado com sucesso!', 'success');
     } else {
-      console.log(response.messages?.join(",\n"));
+      console.log(response.messages?.join(',\n'));
     }
     setIsLoading(false);
   }, []);
 
   const handleLogout = useCallback(async () => {
     setAuthenticated(false);
-    localStorage.removeItem("token");
-    Api.defaults.headers.common["Authorization"] = "";
-    setUser({} as IUser);
+    localStorage.removeItem('token');
+    Api.defaults.headers.common['Authorization'] = '';
+    clearUser();
   }, []);
 
   const handleSignUp = useCallback(async (user: IUser) => {
@@ -66,26 +96,87 @@ export const UserProvider: React.FC = ({ children }) => {
 
     const response = await userService.signUp(user);
     if (response.success) {
-      localStorage.setItem("token", JSON.stringify(response.token));
-      Api.defaults.headers.common["Authorization"] = `Bearer ${response.token}`;
-      setUser(response.data);
+      localStorage.setItem('token', JSON.stringify(response.token));
+      Api.defaults.headers.common['Authorization'] = `Bearer ${response.token}`;
+      DefineUser(
+        response.data.id,
+        response.data.firstName,
+        response.data.lastName,
+        response.data.username,
+        response.data.email,
+        response.data.description || '',
+      );
       setAuthenticated(true);
-      Feedback("Cadastro realizado com sucesso!", "success");
+      Feedback('Cadastro realizado com sucesso!', 'success');
     } else {
-      console.log(response.messages?.join(",\n"));
+      console.log(response.messages?.join(',\n'));
     }
     setIsLoading(false);
   }, []);
 
+  const defineUserFirstName = useCallback((value: string) => {
+    setFirstName(value);
+  }, []);
+  const defineUserLastName = useCallback((value: string) => {
+    setLastName(value);
+  }, []);
+  const defineUserUsername = useCallback((value: string) => {
+    setUsername(value);
+  }, []);
+  const defineUserEmail = useCallback((value: string) => {
+    setEmail(value);
+  }, []);
+  const defineUserDescription = useCallback((value: string) => {
+    setDescription(value);
+  }, []);
+
+  const clearUser = useCallback(() => {
+    setId('');
+    setFirstName('');
+    setLastName('');
+    setUsername('');
+    setEmail('');
+    setDescription('');
+  }, []);
+
+  const DefineUser = useCallback(
+    (
+      id: string,
+      firstName: string,
+      lastName: string,
+      username: string,
+      email: string,
+      description: string,
+    ) => {
+      setId(id);
+      setFirstName(firstName);
+      setLastName(lastName);
+      setUsername(username);
+      setEmail(email);
+      setDescription(description);
+    },
+    [],
+  );
+
   return (
     <UserContext.Provider
       value={{
-        user,
+        id,
+        firstName,
+        lastName,
+        username,
+        email,
+        description,
         authenticated,
         isLoading,
         handleLogin,
         handleLogout,
         handleSignUp,
+        defineUserFirstName,
+        defineUserLastName,
+        defineUserUsername,
+        defineUserEmail,
+        defineUserDescription,
       }}
     >
       {children}
