f5d0a358049caa1c7cd12e8989d1495eb5c1a18f
Leonardo da Silva <silvaleonardo.ti@gmail.com>
2022-03-25 11:31:59 -0300

    Simplify user interface and ajust methods return

--- src/shared/services/api/user/User.ts
+++ src/shared/services/api/user/User.ts
@@ -6,55 +6,43 @@ export interface IUser {
     firstName: string;
     lastName: string;
     email: string;
-    links?: string[];
-}
-export interface IUserSignUpReturn extends Omit<IUser, 'links'> {
-    token: string
-    password:string;
+    password?:string;
 }
 
-export interface IUserSignInReturn extends Omit<IUser, 'links'> {
-    token: string
-    password:string;
-}
-
-const signIn = async (email: string, password: string): Promise<IRequestResult<IUserSignInReturn>> => {
-    
-        const result = {
+const signIn = async (email: string, password: string): Promise<IRequestResult<IUser>> => {
+        console.log('> API: signIn()');
+        const response = {
             data: {
                 id: "1",
                 username: "leosilva",
                 firstName: "Leonardo",
                 lastName: "Silva",
                 email: email,
-                links: [''],
-                token: 'jwt-123456',
-                password: password,
-            } as IUserSignInReturn,
+            } as IUser,
+            token:'jwt-123456',
             success: true,
             messages: [],
-        } as IRequestResult<IUserSignInReturn> 
+        } as IRequestResult<IUser> 
         
-        return result;
+        return response;
 
 }
 
-const signUp = async (user: Omit<IUser, 'id'|'links'>): Promise<IRequestResult<IUserSignUpReturn>> => {
-    const result = {
+const signUp = async (user: IUser): Promise<IRequestResult<IUser>> => {
+    const response = {
         data: {
             id:'1', /* dados fakes que virão do backend */
             username: user.username,
             firstName: user.firstName,
             lastName: user.lastName,
             email: user.email,
-            token: 'jwt-123456', /* dados fakes que virão do backend */
-            password: '123456', /* dados fakes que virão do backend */
-        } as IUserSignUpReturn,
+        } as IUser,
+        token:'jwt-123456', /* dados fakes que virão do backend */
         success: true,
         messages: [],
-    } as IRequestResult<IUserSignUpReturn> 
+    } as IRequestResult<IUser> 
     
-    return result;
+    return response;
 }
 
 
