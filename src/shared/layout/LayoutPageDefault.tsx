c0337991e6dcd9d90bcba10762e1b96e2d96b819
Leonardo da Silva <silvaleonardo.ti@gmail.com>
2022-03-27 17:35:28 -0300

    Verify page to display Header component

--- src/shared/layout/LayoutPageDefault.tsx
+++ src/shared/layout/LayoutPageDefault.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { Header } from '../components/header/Header';
 
 export const LayoutPageDefault: React.FC = ({ children }) => {
+  const location = useLocation();
+  const pathToShowHeader = ['/profile', '/entrar', '/cadastrar'];
+
   return (
     <>
-      <Header />
+      {pathToShowHeader.includes(location.pathname) && <Header />}
       {children}
     </>
   );
