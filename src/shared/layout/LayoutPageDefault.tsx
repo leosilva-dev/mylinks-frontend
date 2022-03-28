efb63546369755806a294187f036195f9d70cec2
Leonardo da Silva <silvaleonardo.ti@gmail.com>
2022-03-27 21:38:12 -0300

    Add tab title name

--- src/shared/layout/LayoutPageDefault.tsx
+++ src/shared/layout/LayoutPageDefault.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Header } from '../components/header/Header';
 
 export const LayoutPageDefault: React.FC = ({ children }) => {
   const location = useLocation();
   const pathToShowHeader = ['/profile', '/entrar', '/cadastrar'];
+
+  const tabTitle = location.pathname.replace('/', '').replace('@/', '@');
+
+  useEffect(() => {
+    document.title = `Mylinks | ${tabTitle}`;
+  }, [location.pathname, tabTitle]);
 
   return (
     <>
